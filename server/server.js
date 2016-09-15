b69a12db201eb56d289b7f26fe5579855444a3f6
Casey Childers <cachilders@gmail.com>
2016-09-14 19:23:48 -0700

    [feature] Write server with key dependencies and structure

---
+++ server/server.js
@@ -0,0 +1,18 @@
+                      require('dotenv').config();
+const express       = require('express');
+const app           = express();
+const partials      = require('express-partials');
+const session       = require('express-session');
+const bodyParser    = require('body-parser');
+const http          = require('http');
+const fs            = require('fs');
+const chai          = require('chai'); // sitepoint.com/unit-test-javascript-mocha-chai/
+
+const port          = process.env.PORT||8080;
+
+app.listen(port);
+console.log('Up and running on ' + port);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.redirect('/index.html'))
