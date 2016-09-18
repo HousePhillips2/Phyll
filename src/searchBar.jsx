33aaa89a8f4e843c75abe4b3219f5f4b73a5c6c0
phoebe <yeyuting18@gmail.com>
2016-09-18 16:12:54 -0700

    [Feature] Add api route in server to fetch plant facts

--- src/searchBar.jsx
+++ src/searchBar.jsx
@@ -1,5 +1,6 @@
 import $ from 'jquery'; //(cannot find jquery in node modules when compiling) npm install jquery
 import {Link} from 'react-router';
+import PlantFacts from './plantFacts.jsx';
 
 /*Come back: implement autosuggest bar*/
 /*Come back: position the search bar in the center of the page*/
@@ -8,10 +9,13 @@ export default class SearchBar extends React.Component {
 
   render() {
     return (
-      <form id="searchform" onSubmit={this._handleSubmit.bind(this)}>
-        <input type="text" id="searchterm" placeholder="Add My Plant" />
-        <button style={{color:'green'}} type="submit" id="search" >Search</button>
-      </form>
+      <div>
+        <form id="searchform" onSubmit={this._handleSubmit.bind(this)}>
+          <input type="text" id="searchterm" placeholder="Add My Plant" />
+          <button style={{color:'green'}} type="submit" id="search" >Search</button>
+        </form>
+        <PlantFacts />
+      </div>
     )
   }
   _handleSubmit(e){
