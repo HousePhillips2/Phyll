e2e7771a244e50a8abeacd0d46696ef9732f420e
Casey Childers <cachilders@gmail.com>
2016-09-17 22:39:35 -0700

    [feature] Modify deployment scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 const gulp             = require('gulp');
 const eslint           = require('gulp-eslint');
-const connect          = require('gulp-connect');
 const nodemon          = require('gulp-nodemon');
 const mocha            = require('gulp-mocha');
 const inject           = require('gulp-inject');
@@ -14,17 +13,8 @@ const WebpackDevServer = require('webpack-dev-server');
 var browserSync        = require('browser-sync');
 var reload             = browserSync.reload;
 
-gulp.task('checkin', () => console.log('From what I can tell I\'m working fine'))
-
-gulp.task('browser-sync', ['webpack','nodemon'], () => {
-  browserSync({
-    proxy: "localhost:8080",  // local node app address
-    port: 8888,  // use *different* port than above
-    notify: true
-  });
-});
-
-gulp.task('nodemon',  (cb) => {
+// fire up the server
+gulp.task('server-sync',  (cb) => {
   let called = false;
   return nodemon({
     script: 'server/server.js',
@@ -46,32 +36,66 @@ gulp.task('nodemon',  (cb) => {
   });
 });
 
-gulp.task('start', ['browser-sync'], function () {
-  gulp.watch(['./server/**/*.*','./src/**/*.*'], ['webpack'], reload);
+// browser-sync loads a separate server at port 8888 for a live view of /dist output
+gulp.task('browser-sync', ['webpack','server-sync'], () => {
+  browserSync({
+    proxy: "localhost:8080",  // local node app address
+    port: 8888,  // use *different* port than above
+    notify: true
+  });
 });
 
+// check all .js and .jsx files in src for common errors
 gulp.task('lint', () => {
-  gulp.src('src/**/*.js')
+  gulp.src('src/**/*.{js,jsx}')
     .pipe(eslint())
     .pipe(eslint.format())
-    .pipe(eslint.failAfterError())
+    .pipe(eslint.failAfterError());
 });
 
-gulp.task('mochaSuite', () => 
-  gulp.src('./server/tests/mocha/*.js', {read: false, require: ['should', 'enzyme', 'chai', 'sinon']})
+// run all tests in server/tests/mocha
+gulp.task('mochaSuite', () => {
+  gulp.src('./server/tests/mocha/*.js', {
+    read: false,
+    require: [
+      'should',
+      'enzyme',
+      'chai',
+      'sinon'
+      ]})
     .pipe(mocha({reporter: 'progress'}))
-);
+});
 
+// process src directory with webpack
 gulp.task('webpack', () => {
   gulp.src('src/app.js')
     .pipe(webpack(require('./webpack.config.js')))
     .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('dev', ['lint', 'mochaSuite', 'start'], ()=> {
-  console.log('That\'s some clean code!')
+// run webpack and start the server
+gulp.task('build', ['webpack'], () => {
+  nodemon({
+    script: 'server/server.js'
+  });
 });
 
-gulp.task('default', ['lint', 'mochaSuite', 'webpack'], () => {
-  console.log('That\'s some good looking code. Proceed.')
-})
+// start and watch the local dev environment with no linting or tests
+gulp.task('start', ['browser-sync'], () => {
+  gulp.watch(['./server/**/*.*','./src/**/*.*'], ['webpack'], reload);
+});
+
+// test and process src directory
+gulp.task('process', ['lint', 'mochaSuite', 'webpack'], () => {
+  console.log('That\'s some good looking code. Proceed.');
+});
+
+// start the local dev environment with linting and tests
+gulp.task('dev', ['process', 'start'], ()=> {
+  console.log('That\'s some clean code!');
+});
+
+// deploy dev branch, run build
+
+
+// deploy master branch, run build
