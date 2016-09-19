22f07bc9d5b54b34097c259ec6d5d3dc02c3f9b9
phoebe <yeyuting18@gmail.com>
2016-09-18 21:09:07 -0700

    [feature] Add iGarden page, everything is working fine

--- src/addPlant.jsx
+++ src/addPlant.jsx
@@ -1,5 +1,6 @@
 import SearchBar from './searchBar.jsx';
 import PlantFacts from './plantFacts.jsx';
+import PlantForm from './plantForm.jsx';
 
 export default class AddPlant extends React.Component {
   constructor() {
@@ -7,24 +8,38 @@ export default class AddPlant extends React.Component {
     this.state={plantFacts:{}};//initate state
   }
 
-  _addMyPlant(){
+  render() {
+    let length = Object.keys(this.state.plantFacts).length;
+    if (length!==0){
+      return (
+        <div>
+          <SearchBar addMyPlant={this._addMyPlant.bind(this)}/>
+          <PlantForm plantName={this.state.plantFacts.Common_Name}/>
+          <PlantFacts plantFacts = {this.state.plantFacts}/>
+        </div>
+      )      
+    }else{
+      return (
+        <div>
+          <SearchBar addMyPlant={this._addMyPlant.bind(this)}/>
+          <PlantFacts plantFacts = {this.state.plantFacts}/>
+        </div>
+      )            
+    }
+  }
+
+  _addMyPlant(plant){
+    console.log(plant, "inside addMyPlant")
     $.ajax({
-      method: 'GET',
+      method: 'POST',
       url: '/plantFacts',
+      json: true,
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify({plant:plant}),
       success: (plantFacts) => {
-        this.setState({plantFacts});
+        console.log(plantFacts, "plantFacts")
+        this.setState({plantFacts: plantFacts[0]});
       }
     });
   }
-
-  render() {
-    console.log(this,'add plant Component');
-    return (
-      <div>
-        <SearchBar will={'hi'} addMyPlant={this._addMyPlant.bind(this)}/>
-        <PlantFacts plantFacts = {this.state.plantFacts}/>
-      </div>
-    )
-  }
-
 }
--- src/layout.jsx
+++ src/layout.jsx
@@ -4,19 +4,22 @@ export default class Layout extends React.Component {
   render(){
     return (
       <div>
-        <div className='menu'>
-          <ul>
-            <li>
-              <Link to='/addPlant'>Add My Plant</Link>
+        <nav className="navbar navbar-light bg-faded" style={{color: 'green'}}>
+          <ul className="nav navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link" href="#"><em>Phyll </em><span className="sr-only">(current)</span></a>
             </li>
-            <li>
-              <Link to='/myDashboard'>My Dashboard</Link>
+            <li className="nav-item">
+              <Link className="nav-link" to='/addPlant' ><strong>Add My Plant</strong></Link>
             </li>
-            <li>
-              <Link to='/garden'>iGarden</Link>
+            <li className="nav-item">
+              <Link className="nav-link" to='/myDashboard'><strong>My Dashboard</strong></Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to='/garden'><strong>iGarden</strong></Link>
             </li>
           </ul>
-        </div>
+        </nav>
         {this.props.children}
       </div>
     )
