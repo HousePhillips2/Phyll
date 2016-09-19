e39ff00e9a4f8ea8548d1387df706fef683f39aa
phoebe <yeyuting18@gmail.com>
2016-09-18 17:13:48 -0700

    [feather] Implement search bar to make api call to fetch plant facts

--- src/app.jsx
+++ src/app.jsx
@@ -7,6 +7,7 @@ import Garden from './garden.jsx';
 import MyDashboard from './dashBoard.jsx';
 import PlantFacts from './plantFacts.jsx';
 import PlantForm from './plantForm.jsx';
+import AddPlant from './addPlant.jsx';
 
 // ES6 CLASS SYNTAX TO CREATE A REACT COMPONENT
 class MyApp extends React.Component {
@@ -15,7 +16,7 @@ class MyApp extends React.Component {
     <Router history={hashHistory}>
     <Redirect from='/' to='/addPlant'/>
       <Route path='/' component={Layout}>
-        <Route path='addPlant' component={SearchBar} />
+        <Route path='addPlant' component={AddPlant} />
         <Route path='myDashboard' component={MyDashboard} />
         <Route path='garden' component={Garden} />
       </Route>
--- src/searchBar.jsx
+++ src/searchBar.jsx
@@ -1,26 +1,29 @@
 import $ from 'jquery'; //(cannot find jquery in node modules when compiling) npm install jquery
-import {Link} from 'react-router';
-import PlantFacts from './plantFacts.jsx';
 
 /*Come back: implement autosuggest bar*/
 /*Come back: position the search bar in the center of the page*/
 
 export default class SearchBar extends React.Component {
-
+  constructor(props) {
+    super(props);
+  }
   render() {
+    console.log(this.props.will);
     return (
       <div>
         <form id="searchform" onSubmit={this._handleSubmit.bind(this)}>
-          <input type="text" id="searchterm" placeholder="Add My Plant" />
+          <input type="text" id="searchterm" placeholder="Add My Plant" ref={input => this._plant = input}/>
           <button style={{color:'green'}} type="submit" id="search" >Search</button>
         </form>
-        <PlantFacts />
       </div>
     )
   }
   _handleSubmit(e){
     e.preventDefault();
-
+    console.log(this)
+    console.log(this._plant.value, "this typein in SearchBar")
+    this.props.addMyPlant();
+    this._plant.value = '';
   }
 
 }
