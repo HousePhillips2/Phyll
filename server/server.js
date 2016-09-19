4303c3a53b4cb68ad8fb10abee2654cb0cae0818
phoebe <yeyuting18@gmail.com>
2016-09-18 22:43:00 -0700

    [add] Implement pg database in server and connect successfully

--- server/server.js
+++ server/server.js
@@ -6,6 +6,18 @@ const session       = require('express-session');
 const bodyParser    = require('body-parser');
 const http          = require('http');
 const fs            = require('fs');
+const pg = require('pg'); //may consider using pg-promise https://github.com/vitaly-t/pg-promise/wiki/Learn-by-Example
+// instantiate a new client
+// the client will read connection information from
+// the same environment variables used by postgres cli tools
+const client = new pg.Client({
+  user: 'wkuxiucjtyzzuc',
+  password: 'kBz9XixE8joEqMGOoGPcZt_R7h',
+  database: 'd8ju6srcedbq7g',
+  host: 'ec2-54-221-253-87.compute-1.amazonaws.com',
+  port: 5432,
+  ssl: true
+});
 
 const port          = process.env.PORT||8080;
 
@@ -64,6 +76,24 @@ app.post('/plantFacts', (req,res) => {
   let result = plantFacts.filter((plant) => plant.Common_Name === name);
   console.log(result);
   res.send(result);
-})
+});
+
+// connect to our database
+client.connect(function (err) {
+  if (err) throw err;
+
+  // execute a query on our database
+  client.query('SELECT id, user_name from api.users', function (err, result) {
+    if (err) throw err;
+
+    // just print the result to the console
+    console.log(result.rows[1]); // outputs: { user_name: 'Phoebe Maio' }
+
+    // disconnect the client
+    client.end(function (err) {
+      if (err) throw err;
+    });
+  });
+});
 
 
