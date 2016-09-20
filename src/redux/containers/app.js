101c74d4145a456549735998d93e2216175c64c3
Eric Churchill <eric.churchill2@gmail.com>
2016-09-19 19:35:50 -0700

    [feature] Implement basic redux store

---
+++ src/redux/containers/app.js
@@ -0,0 +1,8 @@
+import { List, Map } from 'immutable';
+
+export const INITIAL_STATE = Map();
+
+export const setUsers = (state = INITIAL_STATE, users) => {
+
+  return state.set('users', immutable.List(users));
+};
