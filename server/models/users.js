37c51df2e0b72af9cd6d4f3cab88239178067164
phoebe <yeyuting18@gmail.com>
2016-09-20 13:48:07 -0700

    [update] add basic insert data method for database

--- server/models/users.js
+++ server/models/users.js
@@ -2,13 +2,19 @@
 
 const db = require('./pg-config');
 
+db.none("insert into api.users(user_name) values($1)", ['Eric Churchie'])
+  .then(function () {
+    console.log(success);
+  })
+  .catch(function (error) {
+      // error;
+  });
 
-db.any("select user_name from api.users", [true])// see below for field names in plants table
+db.any("select * from api.users", [true])
   .then(function (data) {
-    console.log(data[1].user_name);//print out user name 'Phoebe Maio'
+      console.log(data);
   })
   .catch(function (error) {
       console.log(error);
   });
-
 
