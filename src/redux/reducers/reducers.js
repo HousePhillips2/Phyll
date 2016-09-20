dc45f7f9f5a10de237621780e5bd0900224be126
Eric Churchill <eric.churchill2@gmail.com>
2016-09-19 19:39:03 -0700

    [refactor] Refactor to ES2016 syntax

--- src/redux/reducers/reducers.js
+++ src/redux/reducers/reducers.js
@@ -1,14 +1,12 @@
-let immutable = require('immutable');
 import { INITIAL_STATE, setUsers } from '../containers/app';
 
-module.exports.reducer = (state = INITIAL_STATE, action) => {
-
+export default reducer = (state = INITIAL_STATE, action) => {
   switch( action.type ){
-
     case 'SET_USERS':
-      return setUsers(state, action.users);
 
+      return setUsers(state, action.users);
     default:
+
       return state;
   }
 };
