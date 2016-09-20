295ecd0049c98ffde43852db7e61e74c47a15259
Eric Churchill <eric.churchill2@gmail.com>
2016-09-19 17:29:55 -0700

    [refactor] Refactor logic into controllers files

---
+++ server/controllers/plant-data.js
@@ -0,0 +1,15 @@
+///////////////    NODE MODULES    ///////////////
+const express = require('express');
+const router  = express.Router();
+
+router.get('/', (req, res) => {
+
+  const plantData = {
+    User: 'Phoebe',
+    Water: 'Medium',
+    Soil: 'pH 7.5',
+    Light: 'Full Sun'
+  };
+
+  res.send(plantData);
+});
