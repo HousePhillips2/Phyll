f0c7522f226a6c27e69ada7cbaa1d49301c0b823
phoebe <yeyuting18@gmail.com>
2016-09-20 13:35:44 -0700

    [refactor] Update database using pg-promise from pg

--- server/models/pg-config.js
+++ server/models/pg-config.js
@@ -1,16 +1,14 @@
-const pg = require('pg'); //may consider using pg-promise https://github.com/vitaly-t/pg-promise/wiki/Learn-by-Example
-
-// instantiate a new client
-// the client will read connection information from
-// the same environment variables used by postgres cli tools
-
-const db = new pg.Client({
+const pgp = require('pg-promise')({
+    // Initialization Options
+});
+const connection = {
   user: process.env.HEROKU_PG_USER,
   password: process.env.HEROKU_PG_PW,
   database: process.env.HEROKU_PG_DB,
   host: process.env.HEROKU_PG_HOST,
   port: 5432,
   ssl: true
-});
+};
+const db = pgp(connection);
 
 module.exports = db;
--- server/models/users.js
+++ server/models/users.js
@@ -1,16 +1,11 @@
+           require('dotenv').config({path: '../../.env'});
+
 const db = require('./pg-config');
 
-// CONNECT to database
-db.connect(err => {
-  if (err) throw err;
-  // execute a query on our database
-  db.query('SELECT id, user_name from api.users', (err, result) => {
-    if (err) throw err;
-    // LOG result to the console
-    console.log(result.rows[1], "query result"); // outputs: { user_name: 'Phoebe Maio' }
-    // DISCONNECT from client
-    db.end(function (err) {
-      if (err) throw err;
+db.any("select * from api.users", [true])
+    .then(function (data) {
+        console.log(data);
+    })
+    .catch(function (error) {
+        console.log(error);
     });
-  });
-});
