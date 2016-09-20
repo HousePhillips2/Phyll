a95b4ce6b4fe9970d31773665d72f4e3ca8d03ad
Casey Childers <cachilders@gmail.com>
2016-09-20 15:31:31 -0700

    [feature] Implement MongoDB phyllOS device db

---
+++ server/models/mdb-config.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const path = require('path');
+
+mongoose.connect(process.env.MONGO_DB_URI);
+
+var mdb = mongoose.connection;
+mdb.on('error', console.error.bind(console, 'Cobra: 1. Mongoose: 0.'));
+mdb.once('open', function () {
+  console.log('Mongoose is alive and kicking.')
+});
+
+module.exports = mdb;
