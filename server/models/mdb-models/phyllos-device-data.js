58614a3adaed088b046633ddbd35b2d8b839b2e9
Casey Childers <cachilders@gmail.com>
2016-09-20 19:46:13 -0700

    [feature] Build core device DB and CRUD methods

--- server/models/mdb-models/phyllos-device-data.js
+++ server/models/mdb-models/phyllos-device-data.js
@@ -14,25 +14,53 @@ const deviceSchema = mongoose.Schema({
   noise       : []
 });
 
-// deviceSchema.methods.record = function record(cb) {
-//   Device.find
-// }
-  // updates record or creates new
-  // up to 43k records (one month at one per minute)
+deviceSchema.methods.record = (req, res) => { // req should be complete post body of device status
+  Device.findOneAndUpdate({deviceId: rec}, {
+    $set: {
+      deviceId    : req.deviceId,
+      deviceOS    : req.deviceOS,
+      deviceAlert : req.deviceAlert
+    }, 
+    $push: {
+      date        : req.date,
+      moisture    : req.moisture,
+      ph          : rec.ph,
+      light       : rec.light,
+      humidity    : rec.humidity,
+      temperature : rec.temperature,
+      pressure    : rec.pressure,
+      noise       : rec.noise
+    }
+  }, {upsert: true}, (err) => {
+    
+  })
+}
 
-// deviceSchema.methods.prune
+// deviceSchema.methods.prune TODO
   // empties device results arrays of all but a week's data (10k at one per minute)
 
-// deviceSchema.methods.purge
+// deviceSchema.methods.purge TODO
   // destroy all environmental data from device
 
-// deviceSchema.methods.retrieve
-  // fetch target device record by id
+deviceSchema.methods.retrieve = (req, res) => {
+  Device.findOne({deviceId: req}).exec((err, device) => { // req should be a device id string
+    if (err) res.status(500).send(err);
+    res.status(200).send(device);
+  });
+};
 
-// deviceSchema.methods.list
-  // fetch a list of all active devices (post within last hour), each bundled with most recent measurements
+deviceSchema.methods.list = (req, res) => {
+  Device.find((err, devices)=> {
+    if (err) res.status(500).send(err);
+    res.status(200).send(devices);
+  });
+};
 
-// deviceSchema.methods.discard
-  // delete device from db
+deviceSchema.methods.discard = (req, res) => { // req should be a device id string
+  Device.findOneAndRemove({deviceId: req}, (err) => {
+    if (err) res.status(500).send(err);
+    res.status(200).send('Device ', req, ' has been removed from phyll.IO');
+  })
+}
 
 module.exports = mongoose.model('Device', deviceSchema);
