7149b9e1409f13443a2d5a0d5d878983b9d8b89e
Casey Childers <cachilders@gmail.com>
2016-09-20 22:28:03 -0700

    [feature] Add purge method to device db

--- server/helpers/rnn.js
+++ server/helpers/rnn.js
@@ -1,14 +1,16 @@
-const CharRnn = require("./char-rnn");
-const Memory = require("./memory");
-const Linalg = require("./linalg");
-const Lstm = require("./lstm");
+//NOPE
 
-const networkMemory = new Memory(buffer, metadata);
+// const CharRnn = require("./char-rnn");
+// const Memory = require("./memory");
+// const Linalg = require("./linalg");
+// const Lstm = require("./lstm");
 
-const linalg = new Linalg(networkMemory);
+// const networkMemory = new Memory(buffer, metadata);
 
-const params = {affines, nNodes, nLayers, vocab, ivocab}
-// ^^^ there are more steps here. see load.js file for more information.
+// const linalg = new Linalg(networkMemory);
 
-const model = new LSTM(linalg, params);
-const myNetwork = new CharRNN(model)
+// const params = {affines, nNodes, nLayers, vocab, ivocab}
+// // ^^^ there are more steps here. see load.js file for more information.
+
+// const model = new LSTM(linalg, params);
+// const myNetwork = new CharRNN(model)
