0caa6f92485dcc5d8230a00c40f553679bddafb0
phoebe <yeyuting18@gmail.com>
2016-09-22 20:06:44 -0700

    [feature] Able to display user data on page from sql database

--- server/controllers/api/garden.js
+++ server/controllers/api/garden.js
@@ -1,17 +1,25 @@
 ///////////////    NODE MODULES    ///////////////
 const express = require('express');
 const router  = express.Router();
+const db      = require('../../models/pg-config.js');
 
 router.get('/', function(req, res) {
 
-  const friends = [
-    {User: 'Phoebe', user_img:'https://figuya.com/uploads/product/profile_picture/6195/profile_Pop-Animation-67-Natsu-Dragneel-Vorschau.jpg', plants: '4 Plants', Light: 'Full Sun'},
-    {User: 'Casey', user_img:'https://figuya.com/uploads/product/profile_picture/6189/profile_Pop-Animation-79-Soul-Vorschau.jpg', plants: '3 Plants', Light: 'Full Sun'},
-    {User: 'Eric', user_img:'https://figuya.com/uploads/product/profile_picture/6188/profile_Pop-Animation-80-Maka-Albarn-Vorschau.jpg', plants: '2 Plants', Light: 'Full Sun'},
-    {User: 'Sergey', user_img:'https://figuya.com/uploads/product/profile_picture/6191/profile_Pop-Animation-72-Sasuke-Uchiha-Vorschau.jpeg', plants: '1 Plants', Light: 'Full Sun'}
-  ];
+  // const friends = [
+  //   {User: 'Phoebe', user_img:'https://figuya.com/uploads/product/profile_picture/6195/profile_Pop-Animation-67-Natsu-Dragneel-Vorschau.jpg', plants: '4 Plants', Light: 'Full Sun'},
+  //   {User: 'Casey', user_img:'https://figuya.com/uploads/product/profile_picture/6189/profile_Pop-Animation-79-Soul-Vorschau.jpg', plants: '3 Plants', Light: 'Full Sun'},
+  //   {User: 'Eric', user_img:'https://figuya.com/uploads/product/profile_picture/6188/profile_Pop-Animation-80-Maka-Albarn-Vorschau.jpg', plants: '2 Plants', Light: 'Full Sun'},
+  //   {User: 'Sergey', user_img:'https://figuya.com/uploads/product/profile_picture/6191/profile_Pop-Animation-72-Sasuke-Uchiha-Vorschau.jpeg', plants: '1 Plants', Light: 'Full Sun'}
+  // ];
+  db.any("select * from api.users")// see below for field names in plants table
+  .then(function (data) {
+    console.log(data);
+    res.send(data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
-  res.send(friends);
 
 });
 
--- src/components/plantForm.jsx
+++ src/components/plantForm.jsx
@@ -18,6 +18,10 @@ export default class PlantForm extends React.Component {
             <input type="text" className="form-control" id="nickname" ref={input => this._plantNickName = input}/>
           </div>
           <div className="form-incline">
+            <label for="deviceId">Phyll No.:</label>
+            <input type="text" className="form-control" id="deviceId" ref={input => this._deviceId = input}/>
+          </div>
+          <div className="form-incline">
             <label for="image">Image:</label>
             <input type="file" className="form-control" id="image" />
           </div>
@@ -28,25 +32,24 @@ export default class PlantForm extends React.Component {
   }
   _handleSubmit(e){
     e.preventDefault();
-    let user_name = 'Phoebe Y Maio';
+    let user_name = 'Casey';
     let oauth_key = 1;
     let email = 'aaaaaaaa@gmail.com';
-    let user_img = 'http://www.ikea.com/ie/en/images/range-introduction/ikea-plant-and-pot__1364299621645-s4.jpg';
-    let deviceId = 100;
-    let plant_img ='http://gardeners.s3.amazonaws.com/p/VETOM30892_3.jpg';
+    let user_img = 'https://figuya.com/uploads/product/profile_picture/6189/profile_Pop-Animation-79-Soul-Vorschau.jpg';
+    let plant_img ='http://cdn1.bigcommerce.com/server4100/6ys4nr/product_images/uploaded_images/money-tree-bonsai-tree.jpg';
     //later on, use Oauth id to get user info (i.e. user_name, oauth_key, email, img) and send it along with the ajax request below;
-    this._addPlant(this._plantName.value, this._plantNickName.value, this.props.plantId,user_name,oauth_key,email,user_img,plant_img);
-    //console.log(this._plantName.value, this._plantNickName.value, this.props.plantId,'inside add plant input submit')
+    this._addPlant(this._plantName.value, this._plantNickName.value, this.props.plantId,this._deviceId.value, user_name,oauth_key,email,user_img,plant_img);
+    console.log(this._plantName.value, this._plantNickName.value, this.props.plantId,this._deviceId,'inside add plant input submit')
 
     this.context.router.push('/myDashboard');
   }
-  _addPlant(plantName,plantNickName,plantId, user_name, oauth_key,email, user_img, plant_img){
+  _addPlant(plantName,plantNickName,plantId, deviceId,user_name, oauth_key,email, user_img, plant_img){
     $.ajax({
       method: 'POST',
       url: '/api/plantData',
       json: true,
       contentType: 'application/json; charset=utf-8',
-      data: JSON.stringify({plantName, plantNickName,plantId,user_name, oauth_key,email, user_img, plant_img}),
+      data: JSON.stringify({plantName, plantNickName,plantId,deviceId,user_name, oauth_key,email, user_img, plant_img}),
       success: (data) => {
         console.log('redirect me to dashboard');
       }
