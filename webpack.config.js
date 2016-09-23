1ad1504abece33322a8c22105e7bc8e7036c18b9
Casey Childers <cachilders@gmail.com>
2016-09-23 11:39:17 -0700

    [refactor] Add loaders to webp[ack config

--- webpack.config.js
+++ webpack.config.js
@@ -23,14 +23,38 @@ const config = {
       test: /\.jsx$|\.js$/,
       loader: 'babel',
       query: {
-        presets:['es2015','react']
+        presets:['es2015','react', 'stage-0']
       },
       exclude: [nodeModulesPath]
     },
     {
       test: /\.css$/,
       loader: 'style!css'
-    }]
+    },
+    { 
+        test: /\.png$/, 
+        loader: "url-loader?limit=100000" 
+      },
+      { 
+        test: /\.jpg$/, 
+        loader: "file-loader" 
+      },
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url?limit=10000&mimetype=application/octet-stream'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'file'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url?limit=10000&mimetype=image/svg+xml'
+      }]
   },
   plugins: [
     new HtmlWebpackPlugin({
