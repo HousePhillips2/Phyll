e96e263d650793766004eb1b084b42a1df48dfe5
Eric Churchill <eric.churchill2@gmail.com>
2016-09-22 23:26:32 -0700

    [refactor] Complete mounting Express sub-apps to main app

--- server/controllers/io/io.js
+++ server/controllers/io/io.js
@@ -3,9 +3,17 @@ const express     = require('express');
 const router      = express.Router();
 
 //////////////    SERVER MODULES    //////////////
+const Device        = require('../../models/mdb-models/phyllos-device-data.js');
+const db            = require('../../models/pg-config');
+const mdb           = require('../../models/mdb-config.js');
 
 
-
+router.post('/record', (req, res) => {Device.record(req.body.status, res);});
+router.post('/retrieve', (req, res) => {Device.retrieve(req.body.deviceId, res);});
+router.get('/list', (req, res) => {Device.list(req, res);});
+router.post('/prune', (req, res) => {Device.prune(req.body.deviceId, res);});
+router.post('/discard', (req, res) => {Device.discard(req.body, res);});
+router.post('/purge', (req, res) => {Device.purge(req.body, res);});
 
 
 module.exports = router;
