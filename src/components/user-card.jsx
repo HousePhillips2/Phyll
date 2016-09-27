94342804befae05b8e9bf6a11db71c2df14b12a3
Casey Childers <cachilders@gmail.com>
2016-09-27 00:16:07 -0700

    [style] Minor tweaks to provisional cards

--- src/components/user-card.jsx
+++ src/components/user-card.jsx
@@ -11,31 +11,33 @@ export default class userCard extends React.Component {
     const user = this.props.user;
     return(
       // TODO: DEFINE CSS styles for user card
-      <div className="card-group">
-        <div className="card card-inverse card pull-left" style={{ border: 0 }}>
-          <img className="card-img-top media-middle text-xs-center" style={ {width: '150px', height: '150px'} } src={ user.userImg }/>
-          <div className="card-block">
-            <h4 className="card-footer head text-success text-xs-center">{`${ user.userName }`}</h4>
-            {/*<p className="card-text graff">Some text of some sort</p>*/}
+      <div className="card-deck-wrapper">
+        <div className="card-deck">
+          <div className="card card-inverse card pull-left" style={{ border: 0 }}>
+            <img className="card-img-top media-middle text-sm-center" style={ {width: '150px', height: '150px'} } src={ user.userImg }/>
+            <div className="card-block">
+              <h4 className="card-footer head text-success text-sm-center">{`${ user.userName }`}</h4>
+              {/*<p className="card-text graff">Some text of some sort</p>*/}
+            </div>
           </div>
-        </div>
-        <div className="card center-block" style={{ border: 0 }}>
-          <div className="card-block media-middle text-xs-center center-block">
-            <h1 className=""><i className="fa fa-heart fa-lg head text-danger"></i></h1>
+          <div className="card center-block" style={{ border: 0 }}>
+            <div className="card-block media-middle text-sm-center center-block">
+              <h1 className=""><i className="fa fa-heart fa-lg head text-danger"></i></h1>
+            </div>
           </div>
-        </div>
-        <div className="card pull-right" style={{ border: 0 }}>
-          { user.plants.slice(0, 3).map( plant => {
-            return(
-              <div className="card card-inverse card" style={{ border: 0 }}>
-                <img className="card-img-top media-middle text-xs-center" style={ {width: '150px', height: '150px'} } src={ plant.img }/>
-                <div className="card-block">
-                  <h4 className="card-footer head text-success text-xs-center">{`${ plant.name }`}</h4>
-                  {/*<p className="card-text graff">Some text of some sort</p>*/}
+          <div className="card pull-right" style={{ border: 0 }}>
+            { user.plants.slice(0, 3).map( plant => { // This needs to just be one plant for the main view card. The dash will have all user plants.
+              return(
+                <div className="card card-inverse card" style={{ border: 0 }}>
+                  <img className="card-img-top media-middle text-sm-center" style={ {width: '150px', height: '150px'} } src={ plant.img }/>
+                  <div className="card-block">
+                    <h4 className="card-footer head text-success text-sm-center">{`${ plant.name }`}</h4>
+                    {/*<p className="card-text graff">Some text of some sort</p>*/}
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
       </div>
     )
