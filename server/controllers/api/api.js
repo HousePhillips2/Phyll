72741bc56e965b6b9f1473ac52193e6596ac9f02
Casey Childers <cachilders@gmail.com>
2016-09-28 11:16:12 -0700

    [merge conflicts]

--- server/controllers/api/api.js
+++ server/controllers/api/api.js
@@ -3,26 +3,18 @@ const express     = require('express');
 const router      = express.Router();
 
 //////////////    SERVER MODULES    //////////////
-const admin      = require('./admin');
-const garden      = require('./garden');
-const plantData   = require('./plant-data');
-const plantFacts  = require('./plant-facts');
-<<<<<<< 99f4f919e8b9bafeaef958d3cb71548434495843
+const admin         = require('./admin');
+const garden        = require('./garden');
+const plantData     = require('./plant-data');
+const plantFacts    = require('./plant-facts');
 const plantsLibrary = require('./plants-library');
-=======
-const auth        = require('./auth-config');
-
->>>>>>> [refactor] move auth api into api model
+const auth          = require('./auth-config');
 
 // ROUTE requests
 router.use('/admin', admin);
 router.use('/garden', garden);
-<<<<<<< 99f4f919e8b9bafeaef958d3cb71548434495843
 router.use('/plantsLibrary', plantsLibrary);
-=======
 router.use('/auth', auth);
-
->>>>>>> [refactor] move auth api into api model
 
 // TODO: SPLIT INTO SUB ROUTES?
 router.use('/plantData', plantData);
