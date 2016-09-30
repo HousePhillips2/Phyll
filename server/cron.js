942ce4051656a17b8f02d347410332749038c6c9
Sergey Potashnikov <sdp781@gmail.com>
2016-09-30 12:03:15 -0700

    [FEATURE] implement daily health-status cron job

---
+++ server/cron.js
@@ -0,0 +1,22 @@
+///////////////    NODE MODULES    ///////////////
+
+const request = require('request');
+const CronJob = require('cron').CronJob;
+
+// DEFINE recurring Cron Job
+
+const daily = new CronJob('1-60/5 * * * * 1-7',
+  function() {
+  console.log('Every 5 seconds, a bunny is murdered');
+}, null, true, 'America/Los_Angeles');
+
+
+
+//TEST if cron job pattern is valid -- (copy/paste your cron below)
+try {
+//     new CronJob('1-60/5 * * * * 1-7', function() {
+//   console.log('Every 5 seconds, a bunny is murdered');
+// }, null, true, 'America/Los_Angeles');
+} catch(ex) {
+    console.log("cron pattern not valid");
+}
