574fd0b2dea5d98e7c8f338bd1cea8e6e4bc44b2
Casey Childers <cachilders@gmail.com>
2016-09-30 16:22:12 -0700

    [feature] Refactor chat design and core feedback

--- src/components/chatbot.jsx
+++ src/components/chatbot.jsx
@@ -5,32 +5,42 @@ import $ from 'jquery';
 const socket = io();
 export default class Chatbot extends React.Component {
   constructor(props) {
-    super(props);
+    super(props);``
     this.state = {
-      value: []
-    };
+      counter: 0,
+      messages: [['Hello ' + this.props.userName + ', What a wonderful day, is it?', 0, 'list-group-item list-group-item-success']],
+      lastMessage: null
+    }
   }
   render() {
     if(this.props.userName!==''){
-      let messages = this.state.value;
+      let messages = this.state.messages;
       //console.log(messages,'update this.state.');
       return (
-        <div className='chatContainer' style={{border:'1px solid green'}}>
-          <ul id='messages'>
-            <li style={{listStyleType: 'none'}}> { 'Hello ' + this.props.userName + ', What a wonderful day, is it?'} </li>
-            {messages.map((msg,index) => <li style={{listStyleType: 'none'}} key={index}>{msg}</li>)}
-            {/*<div>{keys.map(key=> <li>{`${key}: ${plantFacts[key]}`}</li>)}</div>*/}
-          </ul>
-          <div className="update">
+        <div className='container-fluid graff'>
+          <div>
             <form onSubmit={this._notifyServer.bind(this)}>
-              <input style={{width: '450px'}} id='input' type="text" placeholder="Talk to your plant ..." ref={input => this._msg = input}/>
-              <button>Send</button>
+              <div className="input-group">
+                <input type="text" className="form-control" id='input' placeholder="Protip: plants aren't known for wit." ref={input => this._msg = input}/>
+                <span className="input-group-btn">
+                  <button className="btn btn-success" type="submit" >Say it</button>
+                </span>
+              </div>
             </form>
+          </div>
+          <div className="card" style={{ marginTop: 2 + 'rem' }}>
+            <div className="card-body">
+              <div id='messages' className="list-group list-group-flush">
+                {/*<a href="#" className="list-group-item list-group-item-action list-group-item-success">{ 'Hello ' + this.props.userName + ', What a wonderful day, is it?'}</a>*/}
+                {messages.map((msg) => <a href="#" className={msg[2]} key={msg[1]}>{msg[0]}</a>)}
+                {/*<div>{keys.map(key=> <li>{`${key}: ${plantFacts[key]}`}</li>)}</div>*/}
+              </div>
+            </div>
           </div>
         </div>
       );
     } else {
-      return <div >Login to chat with your plant!</div>;
+      return <div className="list-group"><a href="#" className="list-group-item list-group-item-action list-group-item-success">Login to chat with your plant!</a></div>;
     }
   }
   _notifyServer(e){
@@ -40,14 +50,16 @@ export default class Chatbot extends React.Component {
     this._onUpdate(this._msg.value);
     $('#input').val('');
     socket.on('plant', (msg) => {
-      if(msg!== this.state.value[this.state.value.length-1]){
+      if (msg[0] !== this.state.lastMessage) {
         this._onUpdate(msg);
       }
     });
   }
   _onUpdate (msg) {
-    let newMessages = this.state.value.concat(msg);
-    this.setState({value: newMessages });
+    let newMessages = this.state.messages;
+    let counter = this.state.counter + 1;
+    newMessages.unshift([msg, counter, this.state.counter % 2 === 0 ? 'list-group-item' : 'list-group-item list-group-item-success']);
+    this.setState({messages: newMessages.slice(0,7), counter: counter, lastMessage: msg[0]});
   }
 }
 
