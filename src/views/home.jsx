8e242f65ccc56d985aa0e1e2c0b82edc5bbd50b7
phoebe <yeyuting18@gmail.com>
2016-09-29 17:03:05 -0700

    [fix] resolve merge conflict

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -58,15 +58,12 @@ export default class Home extends React.Component {
             <Users users={ this.state.admin }/>
           </div>
           <div className="content-2 col-md-6">
-<<<<<<< 0153dfdc573c70fe662639dfa5ae3e27b92c7e16
             <div>
               <Map/>
             </div>
-=======
             <h1>More content</h1>
             <p>Lorem ipsum</p>
             <Chatbot userName={this.state.userName}/>
->>>>>>> [feature] Add chatbot component in home view
           </div>
         </div>
         <div className="row footer">
