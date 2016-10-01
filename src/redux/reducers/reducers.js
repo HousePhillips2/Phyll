37c3bebda386fa6c729f17f2cd06effa60b47893
Eric Churchill <eric.churchill2@gmail.com>
2016-10-01 15:43:45 -0700

    [feature] Write reducer case for logout action

--- src/redux/reducers/reducers.js
+++ src/redux/reducers/reducers.js
@@ -1,13 +1,14 @@
-import { INITIAL_STATE, setUser, setUserData } from '../containers/app';
+import { INITIAL_STATE, setUser, setUserData, removeUser } from '../containers/app';
 
 export default function reducer(state = INITIAL_STATE, action){
 
   switch( action.type ){
     case 'SET_USER':
       return setUser(state, action.user);
+    case 'REMOVE_USER':
+      return removeUser(state);
     case 'SET_USER_DATA':
       return setUserData(state, action.userData);
-
     default:
       return state;
   }
