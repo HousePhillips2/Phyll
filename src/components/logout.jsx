1048b6a1da37c279f61a6afe0c810d90a42aed87
Eric Churchill <eric.churchill2@gmail.com>
2016-10-01 15:53:27 -0700

    [style] Update style of Logout component

--- src/components/logout.jsx
+++ src/components/logout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+
 export default class Logout extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
+    console.log('this.props.logout:', this.props.logout);
     return (
       <li className="nav-item">
-        <a className="nav-link active graff" href="#" onClick={this.props.logout}>Logout</a>
+        <a className="nav-link active graff" href="#" onClick={ this.props.logout }>Logout</a>
       </li>
     );
   }
