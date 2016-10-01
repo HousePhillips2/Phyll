a5d38e9f1bd946c98428bfd4d2bdac70d184370a
Casey Childers <cachilders@gmail.com>
2016-10-01 11:38:25 -0700

    [style] Add health feedback

--- src/components/user-card.jsx
+++ src/components/user-card.jsx
@@ -12,17 +12,20 @@ export default class userCard extends React.Component {
   render() {
     const user = this.props.user;
     const firstName = user.user_name.split(' ')[0] + '\'s'
+    // TODO: This score will be dynamically populated using n full hearts with the remainder of five hollow
+    const score = <span className="text-danger"><i className="fa fa-heart"></i> <i className="fa fa-heart"></i> <i className="fa fa-heart-o"></i> <i className="fa fa-heart-o"></i> <i className="fa fa-heart-o"></i></span>
     return(
       <div className="card">
         <div className="card-block">
           <div className="media">
-            <a className="media-left"><img className="img-rounded" style={ {width: '125px', height: '125px'} } src={ user.img }/></a>
+            <a className="media-left"><img className="img-rounded" style={ {width: '120px', height: '120px'} } src={user.plants[0].img}/></a>
             <div className="media-body">
               <div className="media">
+                <a className="media-left"><img className="img-rounded" style={ {width: '50px', height: '50px'} } src={ user.img }/></a>
                 <div className="media-body graff">
-                  <h4 className="media-heading header">{`${ firstName }`} <span className="text-success">{`${ user.plants[0].name }`}</span></h4>
+                  <h4 className="media-heading">{`${ firstName }`} <span className="text-success">{`${ user.plants[0].name }`}</span></h4>
+                  { score }
                 </div>
-                <a className="media-right"><img className="img-rounded" style={ {width: '75px', height: '75px'} } src={ user.plants[0].img }/></a>
               </div>
             </div>
           </div>
