87b6d48fe8c8762e5a837eafba048a530c011900
Casey Childers <cachilders@gmail.com>
2016-10-01 09:21:08 -0700

    [style] Minor text tweak

--- src/components/user-card.jsx
+++ src/components/user-card.jsx
@@ -19,7 +19,7 @@ export default class userCard extends React.Component {
             <div className="media-body">
               <div className="media">
                 <div className="media-body graff">
-                  <h4 className="media-heading text-success header">{`${ firstName }`} {`${ user.plants[0].name }`}</h4>
+                  <h4 className="media-heading header">{`${ firstName }`} <span className="text-success">{`${ user.plants[0].name }`}</span></h4>
                   Graphs why not.
                 </div>
                 <a className="media-right"><img className="img-rounded" style={ {width: '75px', height: '75px'} } src={ user.plants[0].img }/></a>
