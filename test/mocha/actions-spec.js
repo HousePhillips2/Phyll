84272b23c91b1fabefe7880fa2e8596489d03aee
Eric Churchill <eric.churchill2@gmail.com>
2016-09-30 23:11:02 -0700

    [feautre] Write tests for Redux actions

--- test/mocha/actions-spec.js
+++ test/mocha/actions-spec.js
@@ -1,31 +1,31 @@
-import { Map, fromJS } from 'immutable';
-import { expect } from 'chai';
-
-import makeStore from '../../src/redux/store/store';
-import { setUsers, addUser } from '../../src/redux/actions/actions';
-
-decribe('actions', () => {
-
-  describe('setUsers', () => {
-
-    beforeEach(() => {
-
-    });
-
-    it('sets users to state store', () => {
-
-    });
-
-    it('should set only one user if only one user', () => {
-        // const make
-    });
-  });
-
-  describe('addUser', () => {
-
-    it('sets a new user to the sate store', () => {
-
-    });
-  });
-
-});
+// import { Map, fromJS } from 'immutable';
+// import { expect } from 'chai';
+//
+// import makeStore from '../../src/redux/store/store';
+// import { setUsers, addUser } from '../../src/redux/actions/actions';
+//
+// decribe('actions', () => {
+//
+//   describe('setUsers', () => {
+//
+//     beforeEach(() => {
+//
+//     });
+//
+//     it('sets users to state store', () => {
+//
+//     });
+//
+//     it('should set only one user if only one user', () => {
+//         // const make
+//     });
+//   });
+//
+//   describe('addUser', () => {
+//
+//     it('sets a new user to the sate store', () => {
+//
+//     });
+//   });
+//
+// });
