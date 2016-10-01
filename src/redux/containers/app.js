0e09f049d090c38e1c484ec1ee429fc4513ff317
Eric Churchill <eric.churchill2@gmail.com>
2016-09-30 23:19:42 -0700

    [feature] Add application logic and begin to develop state tree shape

--- src/redux/containers/app.js
+++ src/redux/containers/app.js
@@ -1,7 +1,23 @@
-import { List, Map } from 'immutable';
+import { List, Map, fromJS } from 'immutable';
 
-export const INITIAL_STATE = Map();
 
-export const setUsers = (state = INITIAL_STATE, users) => {
-  return state.set('users', List(users));
+export const INITIAL_STATE = Map({
+  loggedIn: false
+});
+
+export const setUser = (state, user) => {
+
+  // IF user already loggedIn
+  if( state.get('loggedIn', true)){
+    // RETURN state
+    return state;
+  }
+  // UPDATE state
+  return state.set('loggedIn', true)
+              .set('user', Map({
+                firstName: user.firstName,
+                lastName: user.lastName,
+                facebookId: user.facebookId,
+                email: user.email
+              }));
 };
