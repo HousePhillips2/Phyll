efd97f04881726eb935732287c6828b41d131442
Eric Churchill <eric.churchill2@gmail.com>
2016-09-30 23:16:20 -0700

    [feature] Refactor and apply middlewares to Redux store

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,6 +1,35 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
 import reducer from '../reducers/reducers';
 
+
+const logger = store => next => action => {
+  console.log('dispatching', action);
+  let result = next(action);
+  console.log('next state', store.getState());
+  return result;
+};
+
+const crashReporter = store => next => action => {
+  try {
+    return next(action);
+  } catch (err) {
+    console.error('Caught an exception!', err);
+    Raven.captureException(err, {
+      extra: {
+        action,
+        state: store.getState()
+      }
+    });
+    throw err;
+  }
+};
+
 export default function makeStore() {
-  return createStore(reducer);
+  return createStore(
+    reducer,
+    // COMMENT applyMiddleware to prevent logging actions to console
+    applyMiddleware(logger, crashReporter, thunk)
+  );
 }
