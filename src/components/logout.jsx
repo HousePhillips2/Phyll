9b55d219b95532b9acd3846e777e41fbd0ad7d91
Eric Churchill <eric.churchill2@gmail.com>
2016-10-01 15:53:27 -0700

    [style] Update style of Logout component

--- src/components/logout.jsx
+++ src/components/logout.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+
 export default class Logout extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
+    console.log('this.props.logout:', this.props.logout);
     return (
-      <button className="dropdown-item" type="button" onClick={this.props.logout}>Logout</button>
+      <button className="dropdown-item" type="button" onClick={ this.props.logout }>Logout</button>
     );
   }
 }
