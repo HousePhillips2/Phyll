7431a447fa327bdea0011e2ffff39ea94595ad4b
Eric Churchill <eric.churchill2@gmail.com>
2016-10-02 22:55:49 -0700

    [feature] Write asynchronous Redux helper functions

--- src/redux/actions/helpers.js
+++ src/redux/actions/helpers.js
@@ -1,6 +1,6 @@
 import $ from 'jquery';
 
-import { setUser } from './actions';
+import { setUser, setPlants, setAdmin } from './actions';
 
 
 export function _getUser() {
@@ -12,6 +12,29 @@ export function _getUser() {
       if( user ){
         dispatch(setUser(user));
       }
+      // TODO: WRITE failed login action creator
+    });
+  };
+}
+
+export function _getPlants() {
+  return dispatch => {
+    $.ajax({
+      method: 'GET',
+      url: 'api/plantFacts'
+    }).then(plants => {
+      dispatch(setPlants(plants));
+    });
+  };
+}
+
+export function _getAdmin() {
+  return dispatch => {
+    $.ajax({
+      method: 'GET',
+      url: 'api/admin'
+    }).then(admin => {
+      dispatch(setAdmin(admin));
     });
   };
 }
