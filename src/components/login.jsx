ac675d843c1e68f31205f4fa80e53da9113bcc20
Eric Churchill <eric.churchill2@gmail.com>
2016-10-02 18:12:45 -0700

    [feature] Update Login and Logout components to accept appropriate props

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Auth0Lock from 'auth0-lock';
+
+
 export default class Login extends React.Component {
   constructor(props) {
     super(props);
   }
+
   render() {
+    
     return (
       <li className="nav-item">
         <a className="nav-link active graff" href="api/auth/login">Login</a>
--- src/components/logout.jsx
+++ src/components/logout.jsx
@@ -5,7 +5,6 @@ export default class Logout extends React.Component {
     super(props);
   }
   render() {
-    console.log('this.props.logout:', this.props.logout);
     return (
       <li className="nav-item">
         <a className="nav-link active graff" href="#" onClick={ this.props.logout }>Logout</a>
