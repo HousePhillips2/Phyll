6ef0fa5d11c27879c9f59a250a6f236247333a76
Eric Churchill <eric.churchill2@gmail.com>
2016-10-02 18:19:05 -0700

    [feature] Update user state compatibility with Auth0

--- src/redux/containers/app.js
+++ src/redux/containers/app.js
@@ -2,28 +2,28 @@ import { List, Map, fromJS } from 'immutable';
 
 
 export const INITIAL_STATE = Map({
-  loggedIn: false
+  loggedIn: false,
 });
 
 export function setUser(state, user) {
 
   // IF user already loggedIn
-  if( state.get('loggedIn', true)){
+  if( state.get('loggedIn', true) ){
     // RETURN state
     return state;
   }
   // UPDATE state
   return state.set('loggedIn', true)
               .set('user', Map({
-                firstName: user.firstName,
-                lastName: user.lastName,
+                firstName: user.given_name,
+                lastName: user.family_name,
                 facebookId: user.facebookId,
                 email: user.email,
-                image: user.image
+                image: user.picture_large
               }));
 };
 
 export function removeUser(state) {
   return state.set('loggedIn', false)
               .remove('user');
-}
+};
