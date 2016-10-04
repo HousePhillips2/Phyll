4ea73e386bcf47bd3b8881500d451bf019956cd0
Sergey Potashnikov <sdp781@gmail.com>
2016-10-04 16:56:27 -0700

    [UPDATE] updated horticulture algorithm to update health of plant

--- server/controllers/postgres/_cronjobUpdate.js
+++ server/controllers/postgres/_cronjobUpdate.js
@@ -2,25 +2,25 @@
 const db = require('../../models/pg-config');
 
 
-//-----------------------------------THIS FILE IS FOR UPDATING DAILY PHYL DATA -----------------------
+//-----------------------------------THIS FILE IS FOR UPDATING DAILY PHYL/DEVICE DATA -----------------------
 
 
 const cronUpdate = (req, res) => {
 
     const incomingData = req.body;
-    console.log('incoming', incomingData);
+
     for (let device in incomingData){
 
         let deviceId = incomingData[device].deviceId,
-              phyll = incomingData[device];
+            phyll = incomingData[device];
 
+        db.any('SELECT api.users_plants.id, api.users_plants.health, api.users_plants.health_light, api.users_plants.health_moisture, api.plants.water_s, api.plants.light_s FROM api.users_plants, api.plants WHERE api.users_plants.plant_type = api.plants.id AND api.users_plants.device_id = $1', [deviceId]).then(plant => {
 
-        db.any('SELECT api.users_plants.id, api.plants.water_s, api.plants.light_s FROM api.users_plants, api.plants WHERE api.users_plants.plant_type = api.plants.id AND api.users_plants.device_id = $1', [deviceId]).then(plant => {
-            // console.log(plants);
-            console.log('otvet', phyll.moisture, plant);
-            // plant object contains:(1) id (2) water_s (3)light_s
-            // phyll object contains: (1) deviceId (2) light array (3) moisture array
+            console.log('VOT!', plant[0].health_light, plant[0].health_moisture);
+            // PLANT object contains:(1) id (2) health (3) light-health (4) water-health (5) water_s (6)light_s
+            // PHYLL object contains: (1) deviceId (2) light array (3) moisture array
 
+            //
             let healthFlag = 0;
             let waterHealthFlag = 0;
             let lightHealthFlag = 0;
@@ -31,87 +31,98 @@ const cronUpdate = (req, res) => {
             let light = phyll.light.map(el => {
                 return Number(el);
             });
-            console.log('lookie', light, moisture);
+            // ---- define variables representing state of health (either 1-5 or 1-10 range)
+            let hearts  = plant[0].health;
+            let heartsL = plant[0].health_light;
+            let heartsM = plant[0].health_moisture;
 
-            // const conditioner = plant => {
+            // ------ check the water, and adjust condition if necessary ------
+            if (plant[0].water_s === 'low' || plant[0].water_s === 'medium-low'){
+                if (Number(phyll.moisture[1]) > 950){
+                    condition+= 'I\'m drowning! too much water. ';
+                    waterHealthFlag = 1;
+                } else if (Number(phyll.moisture[1]) < 600){
+                    condition+= 'I\'m thirsty, not enough water. ';
+                    waterHealthFlag = 1;
+                }
+            }else if (plant[0].water_s === 'medium'){
+                if (Number(phyll.moisture[1]) > 1000){
+                    condition+= 'I\'m drowning! too much water. ';
+                    waterHealthFlag = 1;
+                } else if (Number(phyll.moisture[1]) < 750){
+                    condition+= 'I\'m thirsty, not enough water. ';
+                    waterHealthFlag = 1;
+                }
+            }else if (plant[0].water_s === 'medium-high' || plant[0].water_s === 'high'){
+                if (Number(phyll.moisture[1]) > 1050){
+                    condition+= 'I\'m drowning! too much water. ';
+                    waterHealthFlag = 1;
+                } else if (Number(phyll.moisture[1]) < 800){
+                    condition+= 'I\'m thirsty, not enough water. ';
+                    waterHealthFlag = 1;
+                }
+            };
 
-                // check the water, and adjust condition if necessary
-                if (plant[0].water_s === 'low' || plant[0].water_s === 'medium-low'){
-                    if (Number(phyll.moisture[1]) > 950){
-                        condition+= 'I\'m drowning! too much water. ';
-                        waterHealthFlag = 1;
-                    } else if (Number(phyll.moisture[1]) < 600){
-                        condition+= 'I\'m thirsty, not enough water. ';
-                        waterHealthFlag = 1;
-                    }
-                }else if (plant[0].water_s === 'medium'){
-                    if (Number(phyll.moisture[1]) > 1000){
-                        condition+= 'I\'m drowning! too much water. ';
-                        waterHealthFlag = 1;
-                    } else if (Number(phyll.moisture[1]) < 750){
-                        condition+= 'I\'m thirsty, not enough water. ';
-                        waterHealthFlag = 1;
-                    }
-                }else if (plant[0].water_s === 'medium-high' || plant[0].water_s === 'high'){
-                    if (Number(phyll.moisture[1]) > 1050){
-                        condition+= 'I\'m drowning! too much water. ';
-                        waterHealthFlag = 1;
-                    } else if (Number(phyll.moisture[1]) < 800){
-                        condition+= 'I\'m thirsty, not enough water. ';
-                        waterHealthFlag = 1;
-                    }
-                };
+            // ----- check the sunlight, and adjust condition if necessary ----
+            if (plant[0].light_s === 'low' || plant[0].light_s === 'medium-low'){
+                if (Number(phyll.light[2]) > 195){
+                    condition+= 'Im burning! too much sun. ';
+                    lightHealthFlag = 1;
+                } else if (Number(phyll.light[2]) < 155){
+                    condition+= 'Im cold! not enough sun. ';
+                    lightHealthFlag = 1;
+                }
+            }else if (plant[0].light_s === 'medium'){
+                if (Number(phyll.light[2]) > 250){
+                    condition+= 'Im burning! too much sun. ';
+                    lightHealthFlag = 1;
+                } else if (Number(phyll.light[2]) < 165){
+                    condition+= 'Im cold! not enough sun. ';
+                    lightHealthFlag = 1;
+                }
+            }else if (plant[0].light_s === 'medium-high' || plant[0].light_s === 'high'){
+                if (Number(phyll.light[2]) > 275){
+                    condition+= 'Im burning! too much sun. ';
+                    lightHealthFlag = 1;
+                } else if (Number(phyll.light[2]) < 195){
+                    condition+= 'Im cold! not enough sun. ';
+                    lightHealthFlag = 1;
+                }
+            };
+        // }
 
-                // check the sunlight, and adjust condition if necessary
-                if (plant[0].light_s === 'low' || plant[0].light_s === 'medium-low'){
-                    if (Number(phyll.light[2]) > 195){
-                        condition+= 'Im burning! too much sun. ';
-                        lightHealthFlag = 1;
-                    } else if (Number(phyll.light[2]) < 155){
-                        condition+= 'Im cold! not enough sun. ';
-                        lightHealthFlag = 1;
-                    }
-                }else if (plant[0].light_s === 'medium'){
-                    if (Number(phyll.light[2]) > 250){
-                        condition+= 'Im burning! too much sun. ';
-                        lightHealthFlag = 1;
-                    } else if (Number(phyll.light[2]) < 165){
-                        condition+= 'Im cold! not enough sun. ';
-                        lightHealthFlag = 1;
-                    }
-                }else if (plant[0].light_s === 'medium-high' || plant[0].light_s === 'high'){
-                    if (Number(phyll.light[2]) > 275){
-                        condition+= 'Im burning! too much sun. ';
-                        lightHealthFlag = 1;
-                    } else if (Number(phyll.light[2]) < 195){
-                        condition+= 'Im cold! not enough sun. ';
-                        lightHealthFlag = 1;
-                    }
-                };
-            // }
+        // ----- if condition WAS adjusted (i.e. adverse conditions exist), flip 'healthFlag' boolean -----
+        // ----- if condition was NOT adjusted (i.e. no adverse conditions), set a positive condition ----
+        if (condition){
+            healthFlag = 1;
+        } else if (!condition){
+            condition += 'I got everything I need! :D';
+        }
 
-            // if condition WAS adjusted (i.e. adverse conditions exist), flip 'healthFlag' boolean
-            // if condition was NOT adjusted (i.e. no adverse conditions), set a positive condition
-            if (condition){
-                healthFlag = 1;
-            } else if (!condition){
-                condition += 'I got everything I need! :D';
-            }
+        // ---- if the hearts are already at 0 (i.e. falsy), then prevent reduction below 0 by resetting flag values
+        if (!hearts) {
+            healthFlag = 0;
+        }
+        if (!heartsL){
+            lightHealthFlag = 0;
+        }
+        if(!heartsM){
+            waterHealthFlag = 0;
+        }
 
-            console.log('condition', condition, plant[0].water_s, plant[0].light_s);
+        // ------ update state of each plant in users_plants table
+          db.none('UPDATE api.users_plants SET health = health-$5, health_light = health_light-$7, health_moisture = health_moisture-$6, body_api = ARRAY[$2], daily_moisture = ARRAY[$3], daily_light = ARRAY[$4] WHERE device_id=$1',
+            [deviceId, condition, moisture, light, healthFlag, waterHealthFlag, lightHealthFlag])
+          .catch((error) => {
+            console.log(error,'insert plant form data error');
+          });
 
-              db.none('UPDATE api.users_plants SET health = health+1, body_api = ARRAY[$2], daily_moisture = ARRAY[$3], daily_light = ARRAY[$4] WHERE device_id=$1',
-                [deviceId, condition, moisture, light])
-              .catch((error) => {
-                console.log(error,'insert plant form data error');
-              });
+        res.send('success');
 
-            res.send('success');
-
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
     }
 
 }
--- server/cron.js
+++ server/cron.js
@@ -6,13 +6,19 @@ const CronJob = require('cron').CronJob;
 // ---------------------- DEFINE RECURRING DAILY CRON-JOB: --------------------------------
 // At 8:30PM daily, retrieve the last 14 hours' (i.e. from 6:30 AM) worth of plant-monitoring data
   // convert data into daily summary (daily low, average, and high readings)
-    //send summary to server in order to save in Postgres DB
+    //send summary to deployed server in order to capture data in Postgres DB
 
 const daily = new CronJob('*/10 * * * * 1-7',
   function() {
-    // query MongoDB
-    request('http://localhost:8889/io/dailyData', function(error, response, body){
-      // extract all existing Phyll devices (currently 2)
+
+    // --------------------------- query MongoDB --------------------------
+
+    // request('http://phyll-dev.herokuapp.com/io/dailyData', function(error, response, body){
+    /* uncomment above ^ for production. Uncomment below for development   */
+    request('http://localhost:8080/io/dailyData', function(error, response, body){
+
+
+    // ----------------- extract all existing Phyll devices (currently 2) ----------
       if (body){
         const bod = JSON.parse(body),
               one = bod[0],
@@ -28,7 +34,7 @@ const daily = new CronJob('*/10 * * * * 1-7',
                 moisture: []
               };
 
-        // identify lowest number from array of numbers
+        // -------- identify lowest number from array of numbers ----------
         const lowest = (arr) => {
           const arrToNumbers = arr.map((element)=>{
             return Number(element);
@@ -38,7 +44,7 @@ const daily = new CronJob('*/10 * * * * 1-7',
           })
         };
 
-        // identify lowest number from array of numbers
+        // -------- identify lowest number from array of numbers ----------
         const highest = (arr) => {
           const arrToNumbers = arr.map((element)=>{
             return Number(element);
@@ -48,7 +54,7 @@ const daily = new CronJob('*/10 * * * * 1-7',
           })
         };
 
-        // calculate an average from array of numbers
+        // ---------- calculate an average from array of numbers ----------
         const avg = (arr) => {
           const arrToNumbers = arr.map((element)=>{
             return Number(element);
@@ -61,7 +67,7 @@ const daily = new CronJob('*/10 * * * * 1-7',
           return data[0] / data[1];
         };
 
-        // reduce an array into the following three measurements within it: (1) lowest (2) average (3) highest
+        // -------- reduce an array into the following three measurements within it: (1) lowest (2) average (3) highest -----
         const filler = (arr) => {
           let result = [];
           result.push(lowest(arr));
@@ -70,7 +76,7 @@ const daily = new CronJob('*/10 * * * * 1-7',
           return result;
         };
 
-        // populate device data for transmission
+        // --------- populate device data for transmission ------------
         device1.deviceId = one.deviceId;
         device1.light = filler(one.light);
         device1.moisture = filler(one.moisture);
@@ -82,8 +88,9 @@ const daily = new CronJob('*/10 * * * * 1-7',
 
 
         var httpRequestOptions = {
-          //******* GO BACK & UPDATE URL to official website http://phyll-dev.herokuapp.com/postgres/daily
+          //******* choose which of the subsequent two lines you want to work with (production vs dev)
           url: 'http://localhost:8080/postgres/daily',
+          // url: 'http://phyll-dev.herokuapp.com/postgres/daily',
           form: {
             device1: device1,
             device2: device2
@@ -98,7 +105,7 @@ const daily = new CronJob('*/10 * * * * 1-7',
   }, null, true, 'America/Los_Angeles');
 
 
-//TEST if cron job pattern is valid -- (copy/paste your cron below)
+//TEST if cron job pattern/syntax is valid -- (copy/paste your cron below replacing the commented-out block)
 try {
 //     new CronJob('*/5 * * * * 1-7', function() {
 //   console.log('Every 5 seconds, a bunny is murdered');
