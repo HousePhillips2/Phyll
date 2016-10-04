a128ac7509b421d2a8665edfb3d42c1913dfe266
phoebe <yeyuting18@gmail.com>
2016-10-03 19:47:48 -0700

    [refactor] update query_plant function to retrieve user's plant info

--- server/models/plants.js
+++ server/models/plants.js
@@ -1,15 +1,22 @@
-           require('dotenv').config( {path: '../../.env'});
 const db = require('./pg-config');
-const plantsData = require('../../src/records.js');// an array of arrays of plants data (107 records)
 
-db.any("select plant_name from api.plants")// see below for field names in plants table
-// db.any("select plant_name from api.plants", [true])// see below for field names in plants table
-//   .then(function (data) {
-//     console.log(data[0].plant_name);//print out the first plant name in the plants table
-//   })
-//   .catch(function (error) {
-//       console.log(error);
-//   });
+
+function query_plant(userId, callback){
+  db.any(`select * from api.user_plant where user_id = ${userId}`)
+  .then((data) => {
+    callback(data); //return user's plant data
+  })
+  .catch((error) => {
+    console.log('user plant does not exist');
+    callback(false);
+  });
+}
+
+function store_plant(plant_obj){
+
+}
+
+module.exports = {query_plant, store_plant};
 
 
 //                    ***************  checking that all plants have a valid 'img' property ************
@@ -21,24 +28,36 @@ db.any("select plant_name from api.plants")// see below for field names in plant
   //     console.log(error);
   // });
 
-  /*
-  Table; plants
-    plant_name:
-    plant_family:
-    water_L:
-    soil_pH:
-    light_L:
-    img:
-    light_S:
-    water_S:
-    soil_S:
-    soil_L:
-    fertilizer_S:
-    fertilizer_L:
-    repotting:
-    humidity_S:
-    humidity_L:
-    poisonous_S:
-    poisonous_L:
-    type: null
-  */
+/*
+Table: plants
+
+  plant_name:
+  plant_family:
+  water_L:
+  soil_pH:
+  light_L:
+  img:
+  light_S:
+  water_S:
+  soil_S:
+  soil_L:
+  fertilizer_S:
+  fertilizer_L:
+  repotting:
+  humidity_S:
+  humidity_L:
+  poisonous_S:
+  poisonous_L:
+  type: null
+*/
+
+/*
+Table: user_plant
+
+  user_id:
+  plant_id:
+  device_id:
+  plant_nickname:
+  plant_img
+  id:
+*/
