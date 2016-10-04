880437643d7f96fb746ca5b23da0d2911b80f1d3
Sergey Potashnikov <sdp781@gmail.com>
2016-10-04 11:03:57 -0700

    [UPDATE] defined postgres update feature within cronjobupdate

--- server/controllers/postgres/_cronjobUpdate.js
+++ server/controllers/postgres/_cronjobUpdate.js
@@ -23,9 +23,12 @@ const cronUpdate = (req, res) => {
         db.any('SELECT api.users_plants.id, api.plants.water_s, api.plants.light_s FROM api.users_plants, api.plants WHERE api.users_plants.plant_type = api.plants.id AND api.users_plants.device_id = $1', [deviceId]).then(plant => {
             // console.log(plants);
             console.log('resultat', plant, phyll);
-            // plant has it's (1) id (2) water_s (3)light_s
-            //phyll has its (1) deviceId (2) light array (3) moisture array
+            // plant object contains:(1) id (2) water_s (3)light_s
+            // phyll object contains: (1) deviceId (2) light array (3) moisture array
+
             let healthFlag = false;
+            let waterHealthFlag = false;
+            let lightHealthFlag = false;
             let condition = '';
             const conditioner = plant => {
 
@@ -33,20 +36,26 @@ const cronUpdate = (req, res) => {
                 if (plant[0].water_s === 'low' || plant[0].water_s === 'medium-low'){
                     if (Number(phyll.moisture[1]) > 950){
                         condition+= 'I\'m drowning! too much water. ';
+                        waterHealthFlag = true;
                     } else if (Number(phyll.moisture[1]) < 600){
                         condition+= 'I\'m thirsty, not enough water. ';
+                        waterHealthFlag = true;
                     }
                 }else if (plant[0].water_s === 'medium'){
                     if (Number(phyll.moisture[1]) > 1000){
                         condition+= 'I\'m drowning! too much water. ';
+                        waterHealthFlag = true;
                     } else if (Number(phyll.moisture[1]) < 750){
                         condition+= 'I\'m thirsty, not enough water. ';
+                        waterHealthFlag = true;
                     }
                 }else if (plant[0].water_s === 'medium-high' || plant[0].water_s === 'high'){
                     if (Number(phyll.moisture[1]) > 1050){
                         condition+= 'I\'m drowning! too much water. ';
+                        waterHealthFlag = true;
                     } else if (Number(phyll.moisture[1]) < 800){
                         condition+= 'I\'m thirsty, not enough water. ';
+                        waterHealthFlag = true;
                     }
                 };
 
@@ -54,24 +63,32 @@ const cronUpdate = (req, res) => {
                 if (plant[0].light_s === 'low' || plant[0].light_s === 'medium-low'){
                     if (Number(phyll.light[2]) > 195){
                         condition+= 'I\'m burning! too much sun. ';
+                        lightHealthFlag = true;
                     } else if (Number(phyll.light[2]) < 155){
                         condition+= 'I\'m cold! not enough sun. ';
+                        lightHealthFlag = true;
                     }
                 }else if (plant[0].light_s === 'medium'){
                     if (Number(phyll.light[2]) > 250){
                         condition+= 'I\'m burning! too much sun. ';
+                        lightHealthFlag = true;
                     } else if (Number(phyll.light[2]) < 165){
                         condition+= 'I\'m cold! not enough sun. ';
+                        lightHealthFlag = true;
                     }
                 }else if (plant[0].light_s === 'medium-high' || plant[0].light_s === 'high'){
                     if (Number(phyll.light[2]) > 275){
                         condition+= 'I\'m burning! too much sun. ';
+                        lightHealthFlag = true;
                     } else if (Number(phyll.light[2]) < 195){
                         condition+= 'I\'m cold! not enough sun. ';
+                        lightHealthFlag = true;
                     }
                 };
             }
 
+            // if condition WAS adjusted (i.e. adverse conditions exist), flip 'healthFlag' boolean
+            // if condition was NOT adjusted (i.e. no adverse conditions), set a positive condition
             if (condition){
                 healthFlag = true;
             } else if (!condition){
@@ -83,10 +100,10 @@ const cronUpdate = (req, res) => {
             db.tx(function (t) {
                 // `t` and `this` here are the same;
                 // creating a sequence of transaction queries:
-                var q1 = this.none('update api.users_plants set daily_moisture=$1 where phyll_id=$2', [moist, deviceId]);
-                var q2 = this.none('update api.users_plants set daily_sun=$1 where phyll_id=$2', [light, deviceId]);
-                var q3 = this.none('update api.users_plants set daily_sun=$1 where phyll_id=$2', [light, deviceId]);
-                var q3 = this.none('update api.users_plants set daily_sun=$1 where phyll_id=$2', [light, deviceId]);
+                var q1 = this.none('update api.users_plants set body_api=$1 where device_id=$2', [condition, deviceId]);
+                var q2 = this.none('update api.users_plants set daily_moisture=$1 where device_id=$2', [light, deviceId]);
+                var q3 = this.none('update api.users_plants set daily_light=$1 where device_id=$2', [light, deviceId]);
+                var q3 = this.none('update api.users_plants set daily_sun=$1 where device_id=$2', [light, deviceId]);
 
                 // returning a promise that determines a successful transaction:
                 return this.batch([q1, q2, q3]); // all of the queries are to be resolved;
