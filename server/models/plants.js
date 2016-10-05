ce199b4e9c3ffd3c152a12a7376248b0fceecb4b
phoebe <yeyuting18@gmail.com>
2016-10-05 12:21:58 -0700

    [fix] resolve conflicts

--- server/models/plants.js
+++ server/models/plants.js
@@ -1,12 +1,3 @@
-<<<<<<< 04b651307640077d63587fd5d2a22ed298410670
-<<<<<<< 470215616f0d2c6eac64604545c03119c9a3c8f3
-=======
-=======
-<<<<<<< f58f4ea9ce8c605e4cc9903a22f985365a7b2073
->>>>>>> [feature] Add routes for device pings
-<<<<<<< 5f598d0637864319328e8b087da0201d02b95931
-           require('dotenv').config( {path: '../../.env'});
->>>>>>> [Update] Insert database with 107 plants data; add basic query function in models
 const db = require('./pg-config');
 
 function query_plant(userId, callback){
@@ -42,24 +33,7 @@ module.exports = {query_plant, store_plant};
   // .catch(function (error) {
   //     console.log(error);
   // });
-=======
-           require('dotenv').config({path: '../../.env'});
 
-=======
->>>>>>> [feature] Add routes for device pings
-const db = require('./pg-config');
-const plantsData = require('../../src/records.js');// an array of arrays of plants data (107 records)
-
-db.any("select plant_name from api.plants")// see below for field names in plants table
-  .then(function (data) {
-    //console.log(data[0].plant_name);//print out the first plant name in the plants table
-  })
-  .catch(function (error) {
-      console.log(error);
-  });
->>>>>>> [Update] Insert database with 107 plants data; add basic query function in models
-
-<<<<<<< 470215616f0d2c6eac64604545c03119c9a3c8f3
 
 /*
 Table: plants
@@ -94,47 +68,4 @@ Table: user_plant
   plant_img
   id:
 */
-=======
-  /*
-  Table; plants
-    plant_name:
-<<<<<<< 5f598d0637864319328e8b087da0201d02b95931
-    plant_family:
-    water_L:
-    soil_pH:
-    light_L:
-    img:
-    light_S:
-    water_S:
-    soil_S:
-    soil_L:
-    fertilizer_S:
-    fertilizer_L:
-    repotting:
-    humidity_S:
-    humidity_L:
-    poisonous_S:
-    poisonous_L:
-    type: null
-  */
-=======
-    plant_family: 
-    water_L: 
-    soil_pH: 
-    light_L: 
-    img: 
-    light_S: 
-    water_S: 
-    soil_S: 
-    soil_L: 
-    fertilizer_S: 
-    fertilizer_L: 
-    repotting: 
-    humidity_S: 
-    humidity_L: 
-    poisonous_S: 
-    poisonous_L: 
-    type: null 
-  */
->>>>>>> [Update] Insert database with 107 plants data; add basic query function in models
->>>>>>> [Update] Insert database with 107 plants data; add basic query function in models
+
