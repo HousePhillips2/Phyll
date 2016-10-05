914040f9c3e5ab13eb4420ec5ca6cc861940e69f
phoebe <yeyuting18@gmail.com>
2016-10-05 10:50:36 -0700

    [feature] connect chatbot with actual plant status to allow chatbot provide dynamic health status answer

--- server/controllers/postgres/retrieve_mood.js
+++ server/controllers/postgres/retrieve_mood.js
@@ -6,17 +6,18 @@ const retrieveMood = (input, callback) => {
         const loggedInUser = input;
 
         db.any('SELECT api.user_plant.plant_nickname, api.user_plant.mood_api, api.user_plant.health, api.user_plant.health_light, api.user_plant.health_moisture FROM api.user_plant, api.users WHERE api.user_plant.user_id = api.users.id AND api.user_plant.user_id = $1', [loggedInUser]).then(healthState => {
-                // gives you access to 'healthstate' -> an array, which contains at index [0] an object with the following properties:
                 //(1) .plant_nickname --> STRING nickname of plant
                 //(2) .mood_api       --> ARRAY containing a string representing the current condition of the plant
                 //(3) .health         --> INTEGER between 1-5 representing the general plant health (hearts)
                 //(4) .health_light   --> INTEGER between 1-10 representing the sunlight health of the plant
                 //(5) .health_moisture -> INTEGER between 1-10 representing the moisture health of the plant
+
             console.log(healthState);
             callback(healthState[0].mood_api);
+        })
     .catch(function (error) {
         console.log(error);
-    };
+    });
   };
 
 
