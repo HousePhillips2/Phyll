c72bff32853f4ce52f4bb9e820cd6a70525eb3f8
Sergey Potashnikov <sdp781@gmail.com>
2016-10-05 10:26:33 -0700

    [UPDATE] added retrieve_mood helper function

--- server/controllers/postgres/retrieve_mood.js
+++ server/controllers/postgres/retrieve_mood.js
@@ -4,7 +4,11 @@ const db = require('../../models/pg-config');
 
 //-----------------------------------THIS FILE IS FOR UPDATING DAILY PHYL/DEVICE DATA -----------------------
 
+<<<<<<< 4b9c2e7f4a70f059630027e80e7f445b1c04886d
 const retrieveMood = (input) => {
+=======
+const retrieveMood = (input, callback) => {
+>>>>>>> [UPDATE] added retrieve_mood helper function
 
         // modify the 'something' below
         const loggedInUser = "b9d56128-8a35-4822-b744-2f57372fe44a";
