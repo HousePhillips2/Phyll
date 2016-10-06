1d01e078d2def9228df2e32635424b016fe4dea7
phoebe <yeyuting18@gmail.com>
2016-10-05 17:00:28 -0700

    [feature] modify api/garden api to retrieve all user info who has plants

--- server/controllers/api/garden.js
+++ server/controllers/api/garden.js
@@ -5,18 +5,12 @@ const db      = require('../../models/pg-config.js');
 
 router.get('/', function(req, res) {
 
-  // const friends = [
-  //   {User: 'Phoebe', user_img:'https://figuya.com/uploads/product/profile_picture/6195/profile_Pop-Animation-67-Natsu-Dragneel-Vorschau.jpg', plants: '4 Plants', Light: 'Full Sun'},
-  //   {User: 'Casey', user_img:'https://figuya.com/uploads/product/profile_picture/6189/profile_Pop-Animation-79-Soul-Vorschau.jpg', plants: '3 Plants', Light: 'Full Sun'},
-  //   {User: 'Eric', user_img:'https://figuya.com/uploads/product/profile_picture/6188/profile_Pop-Animation-80-Maka-Albarn-Vorschau.jpg', plants: '2 Plants', Light: 'Full Sun'},
-  //   {User: 'Sergey', user_img:'https://figuya.com/uploads/product/profile_picture/6191/profile_Pop-Animation-72-Sasuke-Uchiha-Vorschau.jpeg', plants: '1 Plants', Light: 'Full Sun'}
-  // ];
-  db.any("select * from api.users")// see below for field names in plants table
+  db.any("select * from api.user_plant, api.users where api.users.id = api.user_plant.user_id")
   .then(function (data) {
-    res.send(data);
+    res.send(data); //return user info with user's plant info
   })
   .catch(function (error) {
-    console.log(error);
+    console.log(error, 'get users data');
   });
 
 
